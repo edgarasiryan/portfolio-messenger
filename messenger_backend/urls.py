02b55ba527d513e9e08efa843264d3e220ca89c0
Edgar Asiryan <edgarasiryann@gmail.com>
2025-04-05 12:54:28 +0400

    feat(auth): implement JWT authentication
    - Added djangorestframework-simplejwt package
    - Configured token endpoints (/api/token/, /api/token/refresh/)
    - Set ACCESS_TOKEN_LIFETIME = 60 mins

--- messenger_backend/urls.py
+++ messenger_backend/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
     path('chat/', include('chat.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
